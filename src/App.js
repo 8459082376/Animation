a68055075623ced4872a802cacff7d8f42cd58da
Sneha Motghare <snehamotghare1127@gmail.com>
2024-06-23 16:40:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,194 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+
+           <div className='banner'>
+
+                    <div class='testimonal1'>
+
+                    <img class='i1' href="#" src="Ellipse 60.png" />
+
+                    <h4 className='h1'>Jenna</h4>
+                    <p className='p1'> Devops Engineer</p>
+                    <img class='i2' href="#" src="star.png" />
+
+                    <p className='p2'>4.5</p>
+
+
+                    <pre><p className="p3">The  Hubnex  Educate is a  must-<br/>
+                                              have  for any DevOps professional.<br/>
+                                              The  detailed  tutorials on CI/CD <br/> 
+                                              pipelines,   containerization, and <br/>
+                                              cloud    infrastructure    have<br/>
+                                              significantly    enhanced    my <br/> 
+                                              efficiency. The hands-on labs and <br/>
+                                              interactive simulations provide an <br/> 
+                                              immersive   learning  experiences.<br/> 
+                                              making  it easy to implement best <br/> 
+                                              practices in my daily work.</p></pre>
+
+                    </div>
+
+
+
+
+
+
+                    <div class='testimonal2'>
+
+                          <img class='i11' href="#" src="Ellipse 60 (1).png" />
+
+                      <h4 className='h11'>Raj</h4>
+                    <p className='p11'> AIML Engineer</p>
+                       <img class='i21' href="#" src="star.png" />
+
+                        <p className='p21'>4.5</p>
+
+
+                    <pre><p className="p31">For  a  cybersecurity specialist, <br/>
+                                    staying updated is crucial,and the <br/>
+                                    XYZ platform excels in this area.<br/> 
+                                    The  up-to-date courses on threat <br/>
+                                    detection,  network security, and <br/>
+                                    ethical  hacking are comprehensive<br/> 
+                                    and easy to follow.The platform's <br/>
+                                    focus on real-world scenarios and <br/>
+                                    practical   labs has sharpened my <br/>
+                                    skills  and boosted my confidence <br/>
+                                    in tackling security challenges.</p></pre>
+
+
+                                    
+                          </div>
+
+
+
+
+                          <div class='testimonal3'>
+
+                                <img class='ii12' href="#" src="Ellipse 60 (2).png" />
+
+                                 <h4 className='hh12'>Priya</h4>
+                                 <p className='pp12'> Full Stack Developer</p>
+                                   <img class='ii22' href="#" src="star.png" />
+
+                                <p className='pp22'>4.5</p>
+
+
+                                <pre><p className="pp32">For  a  cybersecurity specialist, <br/>
+                                    staying updated is crucial,and the <br/>
+                                    XYZ platform excels in this area.<br/> 
+                                    The  up-to-date courses on threat <br/>
+                                    detection,  network security, and <br/>
+                                    ethical  hacking are comprehensive<br/> 
+                                    and easy to follow.The platform's <br/>
+                                    focus on real-world scenarios and <br/>
+                                    practical  labs has sharpened my <br/>
+                                    skills  and boosted my confidence <br/>
+                                    in tackling security challenges.</p></pre>
+
+
+          
+                               </div> 
+
+                               <div class='testimonal4'>
+
+                                <img class='j1' href="#" src="Ellipse 60 (2).png" />
+
+                                 <h4 className='j2'>Yash</h4>
+                                 <p className='j3'> AI ML Engineer</p>
+                                   <img class='j4' href="#" src="star.png" />
+
+                                <p className='j5'>4.5</p>
+
+
+                                <pre><p className="j6">For  a  cybersecurity specialist, <br/>
+                                    staying updated is crucial,and the <br/>
+                                    XYZ platform excels in this area.<br/> 
+                                    The  up-to-date courses on threat <br/>
+                                    detection,  network security, and <br/>
+                                    ethical  hacking are comprehensive<br/> 
+                                    and easy to follow.The platform's <br/>
+                                    focus on real-world scenarios and <br/>
+                                    practical  labs has sharpened my <br/>
+                                    skills  and boosted my confidence <br/>
+                                    in tackling security challenges.</p></pre>
+
+
+          
+                               </div> 
+
+
+
+                               <div class='testimonal5'>
+
+                                <img class='k1' href="#" src="Ellipse 60 (2).png" />
+
+                                 <h4 className='k2'>Kajol</h4>
+                                 <p className='k3'> Cyber security specialist</p>
+                                   <img class='k4' href="#" src="star.png" />
+
+                                <p className='k5'>4.5</p>
+
+
+                                <pre><p className="k6">For  a  cybersecurity specialist, <br/>
+                                    staying updated is crucial,and the <br/>
+                                    XYZ platform excels in this area.<br/> 
+                                    The  up-to-date courses on threat <br/>
+                                    detection,  network security, and <br/>
+                                    ethical  hacking are comprehensive<br/> 
+                                    and easy to follow.The platform's <br/>
+                                    focus on real-world scenarios and <br/>
+                                    practical  labs has sharpened my <br/>
+                                    skills  and boosted my confidence <br/>
+                                    in tackling security challenges.</p></pre>
+
+
+          
+                               </div> 
+
+
+                               <div class='testimonal6'>
+
+                                <img class='l1' href="#" src="Ellipse 60 (2).png" />
+
+                                   <h4 className='l2'>Yuki</h4>
+                                   <p className='l3'> Full Stack Developer</p>
+                                   <img class='l4' href="#" src="star.png" />
+
+                                    <p className='l5'>4.5</p>
+
+
+                                     <pre><p className="l6">As  a  full stact developer, <br/>
+                                           Hubnex  educate  has  been <br/>
+                                            instrumental  in  keeping  my<br/> 
+                                            skills  current.  The in-depth <br/>
+                                             cources  cover evrything  from <br/>
+                                             front-end   frameworks   to<br/> 
+                                             back-end  server  management.<br/>
+                                             The   projects  and coding <br/>
+                                             challenges  provided  by  the <br/>
+                                            platform are incredibly useful<br/>
+                                             for applying what i've learned <br/>
+                                             and for building a solid <br/>
+                                             portfolio.</p></pre>
+
+
+
+                                             </div> 
+
+
+
+
+                               
+         </div>
+
+    </div>
+  );
+}
+
+export default App;
